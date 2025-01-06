e1b6ce0b813c4c39776b9a5487dae6c9c4e87a34
RoutuPrabhakara <routuprabhakar2000@gmail.com>
2025-01-06 10:00:38 +0530

    'update'

--- src/Components/header/Header.jsx
+++ src/Components/header/Header.jsx
@@ -1,50 +1,65 @@
-import React from 'react'
+import React from "react";
 
 function Header() {
   return (
     <header>
-    <nav className="navbar navbar-expand-sm bg-green logo text-white">
-<div className="container-fluid ">
- 
- <a className="navbar-brand" href="#">
-  <h3 className="fs-3 text-white">Agri</h3>
- </a>
-  <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#collapsibleNavbar">
-    <span className="navbar-toggler-icon"></span>
-  </button>
-  <div className="collapse navbar-collapse " id="collapsibleNavbar">
-   <div className="uldata">
-   <ul className="navbar-nav gap-3    ">
-      <li className="nav-item">
-        <a className="nav-link" href="#">Home</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Marketplace</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Weather</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Education</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Government Schemes.</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">Button</a>
-      </li>
-    </ul>
-   </div>
-  </div>
- 
-</div>
-</nav>
-
-
-
-
-  </header>
-  )
+      <nav className="navbar navbar-expand-lg bg-success navbar-dark ">
+        <div className="container-fluid">
+          {/* Logo */}
+          <a className="navbar-brand d-flex align-items-center" href="#">
+            <h3 className="fs-3 text-white m-0">Agri</h3>
+          </a>
+          {/* Navbar Toggler */}
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#navbarContent"
+            aria-controls="navbarContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          {/* Navbar Links */}
+          <div className="collapse navbar-collapse" id="navbarContent">
+            <ul className="navbar-nav ms-auto gap-3 text-center">
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Home
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Marketplace
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Weather
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Education
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="#">
+                  Government Schemes
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="btn btn-warning text-white px-3 py-1" href="#">
+                  Button
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
